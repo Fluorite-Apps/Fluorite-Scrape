a2096ee10e872acba36fa39f403e25dab134632b
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2022-11-28 14:50:14 +0000

    Base CLI updated so videos can be downloaded

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-# import validators
 import time
-from bs4 import BeautifulSoup
+import youtube_dl
 
 # Main Menu
 def cli_main_menu():
@@ -17,8 +16,8 @@ def cli_main_menu():
     #     print("Invalid Url")
     #     exit()
     # Input scraping option
-    scrape_what = input("What do you want to scrape?\n1 = Text\n2 = Images\n3 = Videos (coming soon)\n")
-    scrape_what_list = ['1', '2']
+    scrape_what = input("What do you want to scrape?\n1 = Text\n2 = Images\n3 = Videos\n")
+    scrape_what_list = ['1', '2', '3']
     # Validate scraping option
     try:
         int(scrape_what)
@@ -32,20 +31,40 @@ def cli_main_menu():
 
 # Scrape the Url
 def cli_scrape(url, scrape_what):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url)
+    except:
+        exit("Invalid Url")
     print("Sleeping")
     time.sleep(5)
     print("Scraping")
     if int(scrape_what) == int(1):
-        print(driver.find_element(By.XPATH, "/html/body").text)
+        try:
+            print(driver.find_element(By.XPATH, "/html/body").text)
+        except:
+            pass
+
     if int(scrape_what) == int(2):
-        resources = driver.execute_script("return window.performance.getEntriesByType('resource');")
-        for resource in resources:
-            if resource['initiatorType'] == 'img':
+        try:
+            resources = driver.execute_script("return window.performance.getEntriesByType('resource');")
+            for resource in resources:
                 print(resource['name'])
+        except:
+            pass
+    if int(scrape_what) == int(3):
+        try:
+            ydl_opts = {}
+            zxt = url.strip()
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([zxt])
+        except:
+            print("We cannot scrape videos from this url currently")
+    print("Not what you were looking for? This program may not be able to scrape this website (yet)")
+
+
 
 
 
